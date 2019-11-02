a0cb9018db443e7ec74841cfa8b2b0ef96c719a1
Cosmask <cosmas.kasumba@gmail.com>
2019-11-02 11:26:40 +0200

    Angular-AlbumStoreProductPage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import{RouterModule, Routes} from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
